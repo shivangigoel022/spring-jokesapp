1104ff7425827cf23ff9842246de92173b54242a
shivangigoel <shivangigoel022@gmail.com>
2020-07-18 21:31:30 +0530

    initial commit

---
+++ src/main/java/com/springframework/jokes/springjokeapp/SpringJokeappApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.jokes.springjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/jokes/springjokeapp/SpringJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.jokes.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
