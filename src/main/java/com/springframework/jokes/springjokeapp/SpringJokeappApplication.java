a85c50c9ed337c6f2dba7a10a959eed39597074a
shivangigoel <shivangigoel022@gmail.com>
2020-07-19 18:40:06 +0530

    spring XML Configuration

--- src/main/java/com/springframework/jokes/springjokeapp/SpringJokeappApplication.java
+++ src/main/java/com/springframework/jokes/springjokeapp/SpringJokeappApplication.java
@@ -3,8 +3,10 @@ package com.springframework.jokes.springjokeapp;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokeappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/springframework/jokes/springjokeapp/config/ChuckConfiguration.java
+++ src/main/java/com/springframework/jokes/springjokeapp/config/ChuckConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+  //  @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
